2a07d8c9b20470f983cac8515fc13d08833ad779
Melissa Yates <melissampeart@gmail.com>
2019-08-21 16:36:10 -0500

    classwork without challenge

---
+++ loopscw.py
@@ -0,0 +1,37 @@
+# # Python: Loops Classwork
+#
+# ### Exercise 1:
+# Print -20 to and including 50. Use any loop you want.
+
+for idx in range(-20,51):
+    print(idx)
+#
+# ### Exercise 2:
+# Create a loop that prints even numbers from 0 to and including 20.Hint: You can find multiples of 2 with (whatever_number % 2 == 0)
+#
+for x in range(0, 21,2):
+    if (x % 2 == 0):
+        print(x)
+
+# ### Exercise 3:
+# Prompt the user for 3 numbers. Then print the 3 numbers along with their average after the 3rd number is entered. Refer to example below replacing ```NUMBER1```, ```NUMBER2```, ```NUMBER3```, and ```THEAVERAGE``` with the actual values.
+#
+NUMBER1 = int(input("Enter a number"))
+NUMBER2 = int(input("Enter a second number"))
+NUMBER3 = int(input("Enter a third number"))
+AVERAGE = (NUMBER1 + NUMBER2 + NUMBER3)/3
+print("The average of " + " "+ str(NUMBER1) + " "+ str(NUMBER2) + " "+ str(NUMBER3) + " is " + " "+ str(AVERAGE))
+# Ex.Output
+# ```
+# The average of NUMBER1, NUMBER2, and NUMBER3 is THEAVERAGE
+# ```
+# ### Exercise 4:
+# Use any loop to print all numbers between 0 and 100 that are divisible by 4.
+#
+for x in range(0, 101):
+    if (x % 4 == 0):
+        print(x)
+# ### Challenge:
+# Password Checker - Ask the user to enter a password. Ask them to confirm the password. If it's not equal,
+# keep asking until it's correct or they enter 'Q' to quit.
+
